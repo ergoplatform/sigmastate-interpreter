7b7c1c586aba4ee9de02af9cf088b665cd09497d
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2025-01-09 23:54:57 +0300

    different ids for getreg and getreg v6

--- sc/shared/src/test/scala/sigmastate/utxo/BasicOpsSpecification.scala
+++ sc/shared/src/test/scala/sigmastate/utxo/BasicOpsSpecification.scala
@@ -3088,7 +3088,7 @@ class BasicOpsSpecification extends CompilerTestingCommons
     if (VersionContext.current.isV6SoftForkActivated) {
       getRegTest()
     } else {
-      an[sigma.exceptions.ConstraintFailed] should be thrownBy getRegTest()
+      an[sigma.validation.ValidationException] should be thrownBy getRegTest()
     }
   }
 
@@ -3109,7 +3109,7 @@ class BasicOpsSpecification extends CompilerTestingCommons
     if (VersionContext.current.isV6SoftForkActivated) {
       getRegTest()
     } else {
-      an[java.nio.BufferUnderflowException] should be thrownBy getRegTest()
+      an[sigma.validation.ValidationException] should be thrownBy getRegTest()
     }
   }
 
