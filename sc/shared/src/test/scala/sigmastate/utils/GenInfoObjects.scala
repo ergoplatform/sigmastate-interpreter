3b1d8aeaa447cf402e80bbd3bebd53ee568e500d
Jozef Koval <jozef.koval@protonmail.ch>
2026-08-31 23:37:48 +0200

    Remove Scala 2.11-only imports and fix outdated compat comments

--- sc/shared/src/test/scala/sigmastate/utils/GenInfoObjects.scala
+++ sc/shared/src/test/scala/sigmastate/utils/GenInfoObjects.scala
@@ -3,7 +3,6 @@ package sigmastate.utils
 import sigma.ast.SMethod
 import sigma.util.PrintExtensions._
 import scala.util.Try
-import Helpers._ // required for Scala 2.11
 
 /** Generate as a console output all InfoObject objects.
   * Those provide stable identifiers to access metadata information.
--- sdk/shared/src/main/scala/org/ergoplatform/sdk/JsonCodecs.scala
+++ sdk/shared/src/main/scala/org/ergoplatform/sdk/JsonCodecs.scala
@@ -20,7 +20,6 @@ import sigma.serialization.{ErgoTreeSerializer, ValueSerializer}
 import sigma.validation.SigmaValidationSettings
 import sigma.{AnyValue, Coll, Colls, Header, PreHeader, SigmaException}
 import sigmastate.eval.{CPreHeader, _}
-import sigmastate.utils.Helpers._   // required for Scala 2.11
 
 import java.math.BigInteger
 import scala.collection.mutable
