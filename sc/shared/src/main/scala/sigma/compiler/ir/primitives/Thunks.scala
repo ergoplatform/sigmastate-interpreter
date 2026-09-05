fbecdc39e34a0b9a95392062b3f89930bc4f001e
Jozef Koval <jozef.koval@protonmail.ch>
2026-09-05 19:33:13 +0000

    Remove unused PassConfig flags and Rewriter parameter from IR mirroring

--- sc/shared/src/main/scala/sigma/compiler/ir/primitives/Thunks.scala
+++ sc/shared/src/main/scala/sigma/compiler/ir/primitives/Thunks.scala
@@ -323,7 +323,7 @@ trait Thunks extends Functions { self: IRContext =>
     */
   def forceThunkDefByMirror[A](th: ThunkDef[A], subst: MapTransformer = MapTransformer.empty()): Ref[A] = {
     val body = th.scheduleIds
-    val t = DefaultMirror.mirrorSymbols(subst, NoRewriting, body)
+    val t = DefaultMirror.mirrorSymbols(subst, body)
     t(th.root)
   }
 
